e8c0d1ee25a4694c9747bc9ccf45e6080e0e79df
gabsdev09 <gabriellimamateu82@gmail.com>
2026-04-15 13:58:19 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,18 @@
+    const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+            observer.unobserve(entry.target);
+        }
+        });
+    },
+    {
+        threshold: 0.14,
+        rootMargin: "0px 0px -40px 0px",
+    }
+    );
+
+    document.querySelectorAll(".reveal").forEach((element) => {
+    observer.observe(element);
+    });
